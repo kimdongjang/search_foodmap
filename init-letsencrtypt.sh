29f1e6dc605f9fd0cdb2badb374480a97844c2d6
jwk <jwk@power21.co.kr>
2022-06-10 16:11:07 +0900

    .

--- init-letsencrtypt.sh
+++ init-letsencrtypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="virtualhertz.hopto.org"
+domains="virtualhertz.hopto.org www.virtualhertz.hopto.org"
 rsa_key_size=4096
 data_path="../certbot"
 email="naru3644@gmail.com" # Adding a valid address is strongly recommended
