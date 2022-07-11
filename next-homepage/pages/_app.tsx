891720e24c1adb0349862b715619566716a6a7b7
JeonWoonKi <naru_oo@naver.com>
2022-07-12 00:41:40 +0900

    asd

--- next-homepage/pages/_app.tsx
+++ next-homepage/pages/_app.tsx
@@ -7,7 +7,7 @@ import { createStore } from "redux";
 import Layout from "../components/Layout";
 import { makeStore, wrapper } from "../modules/store";
 
-import '../styles/globals.scss'
+import "../styles/globals.scss"
 
 declare global {
   interface Window {
