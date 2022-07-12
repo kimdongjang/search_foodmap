4980243d422debacb32b4adb8495ab24c8210489
jwk <jwk@power21.co.kr>
2022-07-12 17:52:14 +0900

    inject document kakao map api

--- next-homepage/pages/_document.tsx
+++ next-homepage/pages/_document.tsx
@@ -1,17 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import React from "react";
 import { useEffect } from 'react';
 import Script from 'next/script'
 
-export default function Document() {
- 
-  return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <script
+            type="text/javascript"
+            src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&libraries=services`}
+          >
+          </script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
